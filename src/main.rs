569cd6391b8fe3538ab1d92b84801b2512580a33
semssk <ssksem015@gmail.com>
2023-09-04 21:36:59 +0100

    created the cards module

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,94 @@ impl eframe::App for MessageDisplayer {
     }
 }
 
+mod cards {
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize)]
+    enum BoxNumber {
+        Box1,
+        Box2,
+        Box3,
+        Box4,
+        Box5,
+        Box6,
+        Box7(u8),
+    }
+
+    impl BoxNumber {
+        fn move_to_next(&self) -> BoxNumber {
+            type BN = BoxNumber;
+            match self {
+                BN::Box1 => BN::Box2,
+                BN::Box2 => BN::Box3,
+                BN::Box3 => BN::Box4,
+                BN::Box4 => BN::Box5,
+                BN::Box5 => BN::Box6,
+                BN::Box6 => BN::Box7(0),
+                BN::Box7(n) => BN::Box7(n + 1),
+            }
+        }
+
+        fn get_next_date_to_show(&self) -> u32 {
+            todo!()
+        }
+    }
+
+    enum AnswerType {
+        Correct,
+        Wrong,
+    }
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize)]
+    pub struct Card {
+        id: usize,
+        question: String,
+        answer: String,
+        current_box: BoxNumber,
+        showing_date: u32,
+    }
+
+    impl Card {
+        fn on_answer(&mut self, answer: AnswerType) {
+            match answer {
+                AnswerType::Correct => {
+                    self.current_box = self.current_box.move_to_next();
+                }
+                AnswerType::Wrong => self.current_box = BoxNumber::Box1,
+            }
+        }
+    }
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize)]
+    pub struct Deck(Vec<Card>);
+
+    impl Deck {
+        pub fn get_deck() -> Self {
+            todo!()
+        }
+        pub fn get_cards(&mut self) -> &mut Vec<Card> {
+            &mut self.0
+        }
+        pub fn add_card(&mut self, card: Card) {
+            let card = Card {
+                id: self.0.len(),
+                ..card
+            };
+            self.0.push(card);
+        }
+        pub fn delete_card(&mut self, id: usize) {
+            self.0.retain(|card| card.id != id);
+        }
+        pub fn update_card(&mut self, card: Card) {
+            for dcard in self.0.iter_mut() {
+                if dcard.id == card.id {
+                    let _ = std::mem::replace(dcard, card);
+                    break;
+                }
+            }
+        }
+    }
+}
+
 struct LeitnerBox {}
 
 impl Default for LeitnerBox {
