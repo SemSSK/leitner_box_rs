4b5b524d507af07f9437943337f8450fdaee0c2d
semssk <ssksem015@gmail.com>
2023-09-04 19:23:04 +0100

    Moved startup_routine to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,6 @@
-use eframe::*;
+mod startup_routine;
 
-mod startup_routine {
-    use std::{
-        env::{self, VarError},
-        fs,
-        path::Path,
-    };
-    fn get_home_path() -> Result<String, VarError> {
-        env::var("HOME")
-    }
-    pub fn run() -> anyhow::Result<()> {
-        create_storage_directory()?;
-        create_storage_file()?;
-        Ok(())
-    }
-    fn check_if_storage_directory_exists(home_path: &str) -> bool {
-        Path::new(&format!("{}/.leitner_box_rs", home_path)).is_dir()
-    }
-    fn create_storage_directory() -> anyhow::Result<()> {
-        let home_path = get_home_path()?;
-        if !check_if_storage_directory_exists(&home_path) {
-            fs::create_dir(&format!("{}/.leitner_box_rs", home_path))?;
-        }
-        Ok(())
-    }
-    fn check_if_storage_file_exists(home_path: &str) -> bool {
-        Path::new(&format!("{}/.leitner_box_rs/data.toml", home_path)).is_file()
-    }
-    fn create_storage_file() -> anyhow::Result<()> {
-        let home_path = get_home_path()?;
-        if !check_if_storage_file_exists(&home_path) {
-            fs::write(&format!("{}/.leitner_box_rs/data.toml", home_path), "")?;
-        }
-        Ok(())
-    }
-}
+use eframe::*;
 
 fn main() -> Result<()> {
     env_logger::init();
