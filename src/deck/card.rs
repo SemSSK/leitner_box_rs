71c486395ad37eb1823c452e84fd98e7c4dfb529
semssk <ssksem015@gmail.com>
2023-09-05 19:01:16 +0100

    Added tests for card

--- src/deck/card.rs
+++ src/deck/card.rs
@@ -7,7 +7,7 @@ pub enum AnswerType {
     Wrong,
 }
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone)]
 pub struct Card {
     pub id: usize,
     pub question: String,
@@ -27,5 +27,61 @@ impl Card {
             .unwrap()
             .as_millis()
             + self.current_box.get_next_wait_time()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    use crate::deck::box_number::BoxNumber;
+
+    use super::Card;
+
+    fn helper_generate_card() -> Card {
+        Card {
+            id: 0,
+            question: String::new(),
+            answer: String::new(),
+            current_box: BN::Box1,
+            showing_date: 0,
+        }
+    }
+
+    type BN = BoxNumber;
+    #[test]
+    fn on_answer_1() {
+        let mut card = helper_generate_card();
+        card.on_answer(super::AnswerType::Correct);
+        assert_eq!(
+            Card {
+                current_box: BN::Box2,
+                showing_date: SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis()
+                    + BN::Box2.get_next_wait_time(),
+                ..card.clone()
+            },
+            card
+        );
+    }
+
+    #[test]
+    fn on_answer_2() {
+        let mut card = helper_generate_card();
+        card.on_answer(super::AnswerType::Wrong);
+        assert_eq!(
+            Card {
+                current_box: BN::Box1,
+                showing_date: SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis()
+                    + BN::Box1.get_next_wait_time(),
+                ..card.clone()
+            },
+            card
+        );
     }
 }
